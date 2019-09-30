ccc24e19d91c78520db4cbc7f498a30240207ef0
Heather-Ridgill <Heather.Ridgill@gmail.com>
2019-09-29 20:51:53 -0700

    Form-showed-Axios-call-didnt

---
+++ smurfs/src/reducers/index.js
@@ -0,0 +1,30 @@
+import {
+  FETCHING_SMURFS_START,
+  FETCHING_SMURFS_SUCCESS,
+  FETCHING_SMURFS_FAILURE
+} from "../actions";
+
+const initialState = {
+  smurfs: [],
+  isFetching: false,
+  error: ""
+};
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCHING_SMURFS_START:
+      return {
+        ...state,
+        isFetching: true,
+        error: ""
+      };
+    case FETCHING_SMURFS_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        smurfs: action.payload
+      };
+    default:
+      return state;
+  }
+};
